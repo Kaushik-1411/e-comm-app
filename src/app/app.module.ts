6f45b7923d2ccbe5b2dc32a0b7a6d968ea3a4513
kaushik shukla <kaushikshukla1411@gmail.com>
2024-04-28 12:16:51 +0530

    making components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ProductComponent } from './product/product.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
+        HomeComponent,
+        ContactUsComponent,
+        ProductComponent,
+        UserProfileComponent,
     ],
     providers: [
         provideClientHydration()
