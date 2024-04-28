69c07c3045dd1228dfce0b6b15653d1d06f2d038
kaushik shukla <kaushikshukla1411@gmail.com>
2024-04-28 22:32:41 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HomeComponent } from './home/home.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { ProductComponent } from './product/product.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { NumberOnlyDirective } from './shared/directives/number-only.directive';
+import { FooterComponent } from './shared/layouts/footer/footer.component';
+import { HeaderComponent } from './shared/layouts/header/header.component';
+import { PageNotFoundComponent } from './shared/layouts/page-not-found/page-not-found.component';
 
 
 @NgModule({
@@ -16,6 +20,10 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
         ContactUsComponent,
         ProductComponent,
         UserProfileComponent,
+        NumberOnlyDirective,
+        FooterComponent,
+        HeaderComponent,
+        PageNotFoundComponent,
     ],
     providers: [
         provideClientHydration()
